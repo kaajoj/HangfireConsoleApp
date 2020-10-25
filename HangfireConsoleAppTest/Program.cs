9c78738be570ae6588cbbc14a5922a527a265689
kaajoj <kaajooj@gmail.com>
2020-10-25 21:12:08 +0100

    Add project files.

---
+++ HangfireConsoleAppTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HangfireConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
